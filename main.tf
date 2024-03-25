0d79c29465b91a58eee2f7ab3e361f2152b68795
Nguyen Dai Duong <duongnd2016@gmail.com>
2024-03-25 21:35:43 +0800

    revert default vpc

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+#data "aws_vpc" "default" {
+#  default = true
+#}
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
@@ -33,7 +33,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
-  vpc_id = data.aws_vpc.default.id
+#  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
