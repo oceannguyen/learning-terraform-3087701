08dfe5145655ba163a7e170a0e568c0f4cf3b33b
Nguyen Dai Duong <duongnd2016@gmail.com>
2024-03-28 21:08:28 +0800

    refactor to use variables

--- variables.tf
+++ variables.tf
@@ -31,70 +31,12 @@ variable "environment" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.9.0"
-  
-  name = "blog"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-
-  image_id           = data.aws_ami.app_ami.id
-  instance_type      = var.instance_type
+variable "asg_min_size" {
+  description = "Minimumn number of instances in the ASG"
+  default     = 1
 }
 
-module "blog_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-
-  name = "blog-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
-
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
-
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  tags = {
-    Environment = "dev"
-  }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
-  name    = "blog"
-
-  vpc_id = module.blog_vpc.vpc_id
-
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+variable "asg_max_size" {
+  description = "Maximumn number of instances in the ASG"
+  default     = 2
 }
