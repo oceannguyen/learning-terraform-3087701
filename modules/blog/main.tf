5b48bb6ab0e7e3d2e07c3afd4695f4334748ab4b
Nguyen Dai Duong <duongnd2016@gmail.com>
2024-09-30 21:13:55 +0800

    destroy

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -22,39 +22,8 @@ module "vpc" {
   }
 }
 
-resource "aws_subnet" "private" {
-  count                   = length(module.vpc.private_subnets)
-  vpc_id                  = module.vpc.vpc_id
-  cidr_block              = module.vpc.private_subnets[count.index]
-  
-  map_public_ip_on_launch = false
-  availability_zone       = element(module.vpc.azs, count.index)
-
-  ipv6_cidr_block         = cidrsubnet(module.vpc.ipv6_cidr_block, 8, count.index) # Associate IPv6 CIDR block
-}
-
-resource "aws_subnet" "public" {
-  count                   = length(module.vpc.public_subnets)
-  vpc_id                  = module.vpc.vpc_id
-  cidr_block              = module.vpc.public_subnets[count.index]
-  
-  map_public_ip_on_launch = true
-  availability_zone       = element(module.vpc.azs, count.index)
-
-  ipv6_cidr_block         = cidrsubnet(module.vpc.ipv6_cidr_block, 8, count.index) # Associate IPv6 CIDR block
-}
-
-# Check if an Egress-Only Internet Gateway already exists before creating a new one.
-data "aws_egress_only_internet_gateway" "existing" {
-  filter {
-    name   = "vpc-id"
-    values = [module.vpc.vpc_id]
-  }
-}
-
 resource "aws_egress_only_internet_gateway" "egw" {
-  count   = length(data.aws_egress_only_internet_gateway.existing.*.id) == 0 ? 1 : 0
-  vpc_id   = module.vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
 
   tags = {
     Name = "egress-only-internet-gateway"
