7a59dd4624a03ddbb02bf9e1860e15906b088540
Ocean Nguyen <ocean.nguyen@grabtaxi.com>
2024-10-23 21:56:11 +0800

    init basic ec2 instance

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -5,9 +5,6 @@ module "vpc" {
   name          = "ocean-aws-for-all-vpc"
   cidr          = "192.168.0.0/20"
   azs           = ["ap-southeast-1a", "ap-southeast-1b"]  # Specify your availability zones
-  
-  # enable_ipv6                                   = true
-  # public_subnet_assign_ipv6_address_on_creation = true
 
   private_subnets = [
     "192.168.0.0/22",  # First private subnet
@@ -23,3 +20,68 @@ module "vpc" {
     Name = "ocean-aws-for-all-vpc"
   }
 }
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP and SSH traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress = {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web Security Group"
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["137112412989"] # Amazon's official AMIs for Amazon Linux
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.amazon_linux.id # Use an appropriate AMI ID for your region
+  instance_type = "t2.micro"
+
+  subnet_id               = module.vpc.public_subnets[0]
+  vpc_security_group_ids  = [aws_security_group.web_sg.id]
+
+  associate_public_ip_address = true
+
+  user_data                   = <<-EOF
+                                  #!/bin/bash
+                                  yum update -y
+                                  yum install -y nginx
+                                  echo "<h1>Instance ID: $(curl -s http://169.254.169.254/latest/meta-data/instance-id)</h1>" > /usr/share/nginx/html/index.html
+                                  systemctl start nginx
+                                  systemctl enable nginx
+                                  EOF
+
+  tags = {
+    Name = "Nginx Instance"
+  }
+}
+
