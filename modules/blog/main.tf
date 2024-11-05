f036f1eaf8e7165dd74f2817c60499fff3152021
Ocean Nguyen <ocean.nguyen@grabtaxi.com>
2024-11-05 21:32:51 +0800

    basic VPC without module

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -1,36 +1,73 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.13.0"  # Specified version
-
-  name          = "awsforall_vpc"
-  cidr          = "192.168.0.0/20"
-  azs           = ["ap-southeast-1a", "ap-southeast-1b"]  # Specify your availability zones
-
-  private_subnets = [
-    "192.168.0.0/22",  # First private subnet
-    "192.168.4.0/22"   # Second private subnet
-  ]
-
-  public_subnets = [
-    "192.168.8.0/22",  # First public subnet
-    "192.168.12.0/22"  # Second public subnet
-  ]
-
-  private_subnet_tags = {
-    Name = "awsforall_private-subnet"
-  }
-
-  public_subnet_tags = {
-    Name = "awsforall_public-subnet"
-  }
-
-  create_igw = true # although this is default but better to set it specifically 
+# Create a VPC
+resource "aws_vpc" "awsforall_vpc" {
+  cidr_block = "192.168.0.0/24"
 
   tags = {
     Name = "awsforall_vpc"
   }
 }
 
+# Create Private Subnets
+resource "aws_subnet" "awsforall_private_subnet_1" {
+  vpc_id = aws_vpc.awsforall_vpc.id
+  cidr_block = "192.168.0.0/26"
+  availability_zone = "ap-southeast-1a"
+}
+
+resource "aws_subnet" "awsforall_private_subnet_2" {
+  vpc_id = aws_vpc.awsforall_vpc.id
+  cidr_block = "192.168.0.64/26"
+  availability_zone = "ap-southeast-1b"
+}
+
+# Create Public Subnets
+resource "aws_subnet" "awsforall_public_subnet_1" {
+  vpc_id = aws_vpc.awsforall_vpc.id
+  cidr_block = "192.168.0.128/22"
+  availability_zone = "ap-southeast-1a"
+}
+
+resource "aws_subnet" "awsforall_public_subnet_2" {
+  vpc_id = aws_vpc.awsforall_vpc.id
+  cidr_block = "192.168.0.192/22"
+  availability_zone = "ap-southeast-1b"
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "awsforall_igw" {
+  vpc_id = aws_vpc.awsforall_vpc.id
+
+  tags = {
+    Name = "awsforall_igw"
+  }
+}
+
+# Create a Route Table for Public Subnets
+resource "aws_route_table" "awsforall_public_r1" {
+  vpc_id = aws_vpc.awsforall_vpc.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.awsforall_igw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+# Associate Route Table with Public Subnets
+resource "aws_route_table_association" "public_association_1" {
+  subnet_id = aws_subnet.awsforall_public_subnet_1.id
+  route_table_id = aws_route_table.awsforall_public_r1.id
+}
+
+resource "aws_route_table_association" "public_association_2" {
+  subnet_id = aws_subnet.awsforall_private_subnet_2.id
+  route_table_id = aws_route_table.awsforall_public_r1.id
+}
+
+# Security Group to Allow HTTP and SSH Traffic
 resource "aws_security_group" "awsforall_web_sg" {
   name        = "awsforall_web_sg"
   description = "Allow HTTP and SSH traffic"
@@ -51,10 +88,10 @@ resource "aws_security_group" "awsforall_web_sg" {
   }
 
   egress {
-    from_port   = 0
+    from_port   = 0 
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # allow all outbound traffic, including responses on ephemeral ports.
   }
 
   tags = {
