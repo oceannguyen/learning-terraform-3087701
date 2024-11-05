1658bef39021b263ad02c529661cc6cfb2f6295c
Ocean Nguyen <ocean.nguyen@grabtaxi.com>
2024-11-05 21:48:12 +0800

    comment outputs.tf subnets

--- modules/blog/outputs.tf
+++ modules/blog/outputs.tf
@@ -2,10 +2,10 @@ output "vpc_id" {
   value = aws_vpc.awsforall_vpc.id
 }
 
-output "public_subnets" {
-  value = aws_vpc.awsforall_vpc.public_subnets
-}
+# output "public_subnets" {
+#   value = aws_vpc.awsforall_vpc.public_subnets
+# }
 
-output "private_subnets" {
-  value = aws_vpc.awsforall_vpc.private_subnets
-}
+# output "private_subnets" {
+#   value = aws_vpc.awsforall_vpc.private_subnets
+# }
